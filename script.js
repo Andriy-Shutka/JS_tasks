40f6c6b635d4aaf0845d10736a7da7a4381c933d
Andriy <shutkaintel1998@gmail.com>
2022-07-07 20:04:38 +0300

    prompt, aler, confirm

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 "use strict";
-alert("hi there");
-const result = confirm("Are you here?");
+
+alert('Are you sure that want to go out?');
+const result = confirm('Yep or not?');
 console.log(result);
+const answer = +prompt('How many old are you?');
+console.log(typeof(answer));
+console.log(answer);
+const answer1 = [];
+answer1[0] = prompt ('What is your name?', '');
+answer1[1] = prompt('How many years are you?', '');
+answer1[2] = prompt ('What is yoyr surname?');
+console.log(answer1[2]);
+
+document.write(answer1);
